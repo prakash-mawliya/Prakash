230894f12e960e1d9929214080017dc6152571d1
Your Name <you@example.com>
2026-03-21 23:41:46 +0530

    Update vite config to support both Vercel and GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Prakash/",
+  // GitHub Pages needs base path, Vercel needs root '/'
+  base: process.env.GITHUB_ACTIONS ? "/Prakash/" : "/",
 })
